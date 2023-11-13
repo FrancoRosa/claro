8b19e28d1281fb6e8911bfaf6a09c83a62a71a8c
FrancoRosa <km115.franco@gmail.com>
2023-11-13 14:07:38 -0500

    Add simple form

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-console.log("cool");
+
 const i_ds = document.getElementById("i_ds");
 const i_us = document.getElementById("i_us");
 const i_da = document.getElementById("i_da");
 const o_ds = document.getElementById("o_ds");
 const o_us = document.getElementById("o_us");
+const calc = document.getElementById("calc");
+const slc = document.getElementById("company");
+
+const options = [
+  "Cicsa Perú Sac",
+  "Cobertura Total",
+  "Comunicaciones Sur Peruana",
+  "Dimera Servicios Multiples",
+  "Inka Cell",
+  "Niza Región Sur",
+  "Telecomunicaciones Megatic",
+  "Xtend"
+];
+let company = options[0]
+
+options.forEach((optionText) => {
+  const option = document.createElement("option");
+  option.value = optionText;
+  option.text = optionText;
+  slc.add(option);
+});
+
 
 let ds = parseFloat(i_ds.value);
 let us = parseFloat(i_us.value);
 let da = parseFloat(i_da.value);
 
+let ous;
+let ods;
+
 const getResults = (us, ds, da) => {
-  const ous = us >= 20 ? us + da * 0.05 : 0;
-  const ods = ds <= 25 ? ds - da * 0.16 : 0;
+  ous = us >= 20 ? us + da * 0.05 : 0;
+  ods = ds <= 25 ? ds - da * 0.16 : 0;
   o_us.innerText = ous.toFixed(2);
   o_ds.innerText = ods.toFixed(2);
   if (ous >= 50) o_us.classList.add("bad");
@@ -20,6 +45,27 @@ const getResults = (us, ds, da) => {
   if (ods <= -4) o_ds.classList.add("bad");
   else o_ds.classList.remove("bad");
 };
+
+const sendResults = async () => {
+  try {
+    await fetch(
+      "https://docs.google.com/forms/d/e/1FAIpQLSeqJexJO8wcrZ6qjMPdrZQU1x7S9BsBW8U6CA7VTm_CqYdAQQ/formResponse?" +
+      new URLSearchParams({
+        "entry.1293228006": ds,
+        "entry.813637407": us,
+        "entry.1773134115": da,
+        "entry.571625077": ous.toFixed(2),
+        "entry.639341194": ods.toFixed(2),
+        "entry.1202637179": company,
+      }),
+      {
+        mode: "no-cors",
+      }
+    );
+  } catch (e) {
+    console.log(e.message);
+  }
+}
 
 const getUserAgent = () => {
   const nav = navigator.userAgent;
@@ -37,19 +83,15 @@ const getUserAgent = () => {
 
 getUserAgent();
 
-i_ds.addEventListener("input", () => {
+calc.addEventListener("click", () => {
   ds = parseFloat(i_ds.value);
-  getResults(us, ds, da);
-});
-
-i_us.addEventListener("input", () => {
   us = parseFloat(i_us.value);
-  getResults(us, ds, da);
-});
-
-i_da.addEventListener("input", () => {
   da = parseFloat(i_da.value);
   getResults(us, ds, da);
+  sendResults()
 });
 
-getResults(us, ds, da);
+slc.addEventListener("change", () => {
+  company = slc.value
+  console.log(company)
+});
